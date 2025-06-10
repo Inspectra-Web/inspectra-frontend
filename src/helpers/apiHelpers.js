619656dbf36d99dc4de2cb4323e7f6b76bbf4dd0
OkFrank12 <cfoonyemmemme@gmail.com>
2025-06-10 07:13:33 +0100

    Updated the backend URL

--- src/helpers/apiHelpers.js
+++ src/helpers/apiHelpers.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 export const apiClient = axios.create({
-  baseURL: "api.inspectraweb.com/api/v1",
+  baseURL: `${import.meta.env.VITE_SOCKET_URL}/api/v1`,
   withCredentials: true,
 });
 
