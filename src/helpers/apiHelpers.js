da574e676e1dcf6e8d6bb45ef33bc3416457e778
OkFrank12 <cfoonyemmemme@gmail.com>
2025-06-10 07:28:01 +0100

    Added API URL in apiClient.js file

--- src/helpers/apiHelpers.js
+++ src/helpers/apiHelpers.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 export const apiClient = axios.create({
-  baseURL: `${import.meta.env.VITE_SOCKET_URL}/api/v1`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
   withCredentials: true,
 });
 
