1d03fa78b2b0880fb2e556424368e33a56265f2e
OkFrank12 <cfoonyemmemme@gmail.com>
2025-07-07 00:26:03 +0100

    Fixed Auth Header Padding

--- src/ui/Container.jsx
+++ src/ui/Container.jsx
@@ -1,6 +1,6 @@
 export function Container({ children }) {
   return (
-    <main className="bg-slate-900 z-10 relative h-[100vh] overflow-auto px-10">
+    <main className="bg-slate-900 z-10 relative h-[100vh] overflow-auto">
       {children}
     </main>
   );
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,7 +3,7 @@ import Logo from "../components/Logo";
 
 export function Header({ description, link, label }) {
   return (
-    <header className="flex justify-between smmobile:flex-col gap-5 bg-slate-900 py-10 items-center sticky top-0 text-white">
+    <header className="flex justify-between smmobile:flex-col gap-5 bg-slate-900 py-10 items-center sticky top-0 text-white px-10">
       <Logo />
       <div className="">
         <span>{description}</span>{" "}
